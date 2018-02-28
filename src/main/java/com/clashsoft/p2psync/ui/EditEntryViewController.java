e5842e4e641fe1b235fdf8b48f3f62881d40c762
Clashsoft <clashsoft@hotmail.com>
2018-02-28 22:29:18 +0100

    Redesign Edit Entry View
    - Added the EditEntryView.fxml file.
    - Added the EditEntryViewController class.

---
+++ src/main/java/com/clashsoft/p2psync/ui/EditEntryViewController.java
@@ -0,0 +1,31 @@
+package com.clashsoft.p2psync.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class EditEntryViewController
+{
+	@FXML
+	private TextField remoteAddressField;
+	@FXML
+	private TextField remotePortField;
+	@FXML
+	private TextField localPathField;
+	@FXML
+	private TextField remotePathField;
+
+	@FXML
+	private void onBrowseAction()
+	{
+	}
+
+	@FXML
+	private void onOKAction()
+	{
+	}
+
+	@FXML
+	private void onCancelAction()
+	{
+	}
+}
